1f505b6f6e17e796b0b5eee3b717c3b2f958c4b8
PeterZhang2015 <zhangchongzheng@gmail.com>
2016-05-12 11:35:04 +0930

    Revert "Merge remote-tracking branch 'origin/PeterZhang2015-patch-1'"
    This reverts commit 663c7414229d94445d3d3f369fdf5f112b7237ab, reversing
    changes made to 5e4c48c7678e48145bf0a2bad1ec2811b428c48f.

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -11,8 +11,6 @@ import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-// Pull request test from github.
     
     var window: UIWindow?
     var bIsLogin: Bool?    // Define the bIsLogin in order to judge whether the user has logged in. If the user has not logged in, it should show MUMainViewController to the user, otherwise, it should show TabBarController to the user.
@@ -632,7 +630,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                         
                                        // let sentInvitationsVC: MUSentInvitationsTableViewController = navigationVC.topViewController as! MUSentInvitationsTableViewController
                                         
-                                        // pull request test for github.
                                         
                                         for section in 0..<sentInvitationsVC.tableView.numberOfSections {
                                             
